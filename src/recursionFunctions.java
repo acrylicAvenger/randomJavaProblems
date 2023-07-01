31f4f043e7e944fed8b253e0f5e848d03d164ee5
acrylicAvenger <20cs077@charusat.edu.in>
2023-07-01 23:20:14 +0530

    updates in code

---
+++ src/recursionFunctions.java
@@ -0,0 +1,100 @@
+public class recursionFunctions {
+    void printSum(int i, int n, int sum){
+        if(i==n){
+            sum += i;
+            System.out.println("The sum using recursion is: " + sum);
+            return;
+        }
+        sum+=i;
+        printSum(i+1, n, sum);
+    }
+
+    void printFactorial(int num, int product){
+        // the product will be 1 in the beginning
+        if (num == 0 || num == 1){
+            System.out.println("The factorial using recursion is: " + product);
+            return;
+        }
+        product *= num;
+        printFactorial(num-1, product);
+    }
+
+    void printFibonacciSequence(int term1, int term2, int n){
+        // term1 and term2 are 0 and 1 in the beginning respectively
+        if (n==0){
+            return;
+        }
+        int term3 = term1 + term2;
+        System.out.print(term3 + " ");
+        printFibonacciSequence(term2, term3, n-1);
+    }
+
+    // print x^n
+    int xRaisedToN(int x, int n){
+        if(n==1)
+            return x;
+        if(n==0)
+            return 1;
+        int nMinus1 = xRaisedToN(x,n-1);
+        x = x * nMinus1;
+        return x;
+    }
+
+    // above code optimized for stack height = logn.
+    int xRaisedToNPart2(int x, int n){
+        if(n==0)
+            return 1;
+        if(n==1)
+            return x;
+        if(n%2==0)
+            return xRaisedToNPart2(x,n/2)*xRaisedToNPart2(x,n/2);
+        else{
+            return x * xRaisedToNPart2(x,(n-1)/2) * xRaisedToNPart2(x,(n-1)/2);
+        }
+    }
+
+    // reverse a string using recursion
+    void reverseString(String string, int index){
+        if(index==0){
+            return;
+        }
+        System.out.println(string.charAt(index));
+        reverseString(string, index-1);
+    }
+
+
+    void towerOFHanoi (int num, String source, String destination, String helper){
+        if(num==1) {
+            System.out.println("Transfer disk " + num + "from " + source + "to " + destination);
+            return;
+        }
+        towerOFHanoi(num-1,source, helper,destination);
+        System.out.println("Transfer disk " + num + "from " + source + "to " + destination);
+        towerOFHanoi(num-1, helper, source, destination);
+    }
+
+    public static void main(String[] args) {
+        recursionFunctions object = new recursionFunctions();
+        object.printSum(1,5,0);
+        object.printFactorial(6,1);
+        object.towerOFHanoi(6,"1","3","2");
+
+        // reverse string
+        String string = "abcdefgh";
+        object.reverseString(string, string.length()-1);
+
+        // fibonacci
+        int a=0,b=1,n=8;
+        System.out.println("The fibonacci sequence is as follows: ");
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        object.printFibonacciSequence(a,b,n);
+        System.out.println();
+
+        // xRaisedToN
+        int exponential = object.xRaisedToN(3,3);
+        System.out.println(exponential);
+
+    }
+
+}
--- src/sortingFunctions.java
+++ src/sortingFunctions.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-class functions {
+
+public class sortingFunctions {
     void bubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length -i - 1; j++) {
@@ -24,15 +25,28 @@ class functions {
         }
         System.out.println("The output of insertion sort is: " + Arrays.toString(array));
     }
-}
 
-public class sortingFunctions {
+    void selectionSort(int[] array){
+        // variable to store the least value
+        int smallestNumber;
+        for(int i=0;i <= array.length-1;i++){
+            smallestNumber = array[i];
+            for(int j=i+1; j<array.length;j++){
+                if(array[j]<smallestNumber)
+                    smallestNumber=array[j];
+            }
+            int temp = array[i];
+            array[i] = smallestNumber;
+            smallestNumber = array[i];
+        }
+        System.out.println("The output of selection sort is: " + Arrays.toString(array));
+    }
     public static void main(String[] args) {
-        functions object = new functions();
+        sortingFunctions object = new sortingFunctions();
         int[] array = {2,5,4,8,9,5,6,3};
         int[] array2 = {10,9,8,7,6,5,4,3,2,1};
-        object.bubbleSort(array2);
+        object.bubbleSort(array);
         object.insertionSort(array2);
+        object.selectionSort(array2);
     }
-
 }
